89c0e6da1e18f9ddceff1d16bfce65ab0d5ae96a
steinuil <steenuil.owl@gmail.com>
2026-05-12 09:25:17 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+
+            rust-analyzer
+            clippy
+            lldb
+            rustfmt
+
+            libclang
+          ];
+        };
+      }
+    );
+}
