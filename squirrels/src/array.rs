e0e5b73b36aa5356a3d8a7f96993f706630c5ca0
steinuil <steenuil.owl@gmail.com>
2026-05-16 12:03:53 +0200

    add Array::set

--- squirrels/src/array.rs
+++ squirrels/src/array.rs
@@ -1,7 +1,8 @@
-use squirrels_sys::{sq_get, tagSQObjectType_OT_ARRAY};
+use squirrels_sys::{SQFalse, sq_get, sq_newslot, tagSQObjectType_OT_ARRAY};
 
 use crate::{
-    FromSquirrel, Integer, Object, PushIntoStack as _, Result, traits::impl_object_traits,
+    CallResult, FromSquirrel, Integer, IntoSquirrel, Object, PushIntoStack as _, Result,
+    get_runtime_error, traits::impl_object_traits,
 };
 
 #[derive(Debug, Clone, PartialEq)]
@@ -24,5 +25,22 @@ impl<'vm> Array<'vm> {
         let val = unsafe { V::from_stack(-1, self.0.sq) };
         self.0.sq.pop(2);
         val.map(Some)
+    }
+
+    pub fn set<T: IntoSquirrel<'vm>>(&self, key: Integer, value: T) -> CallResult<'vm, ()> {
+        self.0.push_into_stack();
+        key.push_into_stack(self.0.sq);
+        value.push_into_stack(self.0.sq);
+
+        let ret = unsafe { sq_newslot(self.0.sq.vm, -3, SQFalse as _) };
+        if ret.is_error() {
+            self.0.sq.pop(3);
+
+            return Err(crate::CallError::Runtime(get_runtime_error(self.0.sq)));
+        }
+
+        self.0.sq.pop(1);
+
+        Ok(())
     }
 }
