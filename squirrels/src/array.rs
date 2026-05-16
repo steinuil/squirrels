dc4c5fbc011fabeb4e778440ec3ab58c252c5159
steinuil <steenuil.owl@gmail.com>
2026-05-16 19:45:38 +0200

    add some more array methods

--- squirrels/src/array.rs
+++ squirrels/src/array.rs
@@ -1,4 +1,6 @@
-use squirrels_sys::{SQFalse, sq_get, sq_newslot, sq_set, tagSQObjectType_OT_ARRAY};
+use squirrels_sys::{
+    SQTrue, sq_arrayappend, sq_arraypop, sq_get, sq_getsize, sq_set, tagSQObjectType_OT_ARRAY,
+};
 
 use crate::{
     CallResult, FromSquirrel, Integer, IntoSquirrel, Object, PushIntoStack as _, Result,
@@ -43,25 +45,102 @@ impl<'vm> Array<'vm> {
 
         Ok(())
     }
+
+    pub fn append<T: IntoSquirrel<'vm>>(&self, value: T) -> CallResult<'vm, ()> {
+        self.0.push_into_stack();
+        value.push_into_stack(self.0.sq);
+
+        let ret = unsafe { sq_arrayappend(self.0.sq.vm, -2) };
+        if ret.is_error() {
+            self.0.sq.pop(3);
+
+            return Err(crate::CallError::Runtime(get_runtime_error(self.0.sq)));
+        }
+
+        self.0.sq.pop(1);
+
+        Ok(())
+    }
+
+    pub fn pop<T: FromSquirrel<'vm>>(&self) -> CallResult<'vm, T> {
+        self.0.push_into_stack();
+
+        let ret = unsafe { sq_arraypop(self.0.sq.vm, -1, SQTrue as _) };
+        if ret.is_error() {
+            self.0.sq.pop(1);
+
+            return Err(crate::CallError::Runtime(get_runtime_error(self.0.sq)));
+        }
+
+        let v = unsafe { T::from_stack(-1, self.0.sq) };
+        self.0.sq.pop(2);
+        Ok(v?)
+    }
+
+    pub fn len(&self) -> Integer {
+        self.0.push_into_stack();
+        let len = unsafe { sq_getsize(self.0.sq.vm, -1) };
+        self.0.sq.pop(1);
+        len
+    }
 }
 
-#[test]
-fn array_get() {
-    use crate::Squirrel;
+#[cfg(test)]
+mod tests {
+    use super::Array;
+    use crate::{CallError, Integer, Squirrel, Value};
 
-    let sq = Squirrel::new(1024);
-    let arr: Array<'_> = sq.eval("return [123, 456, 789]").unwrap();
-    let v: Integer = arr.get(1).unwrap().unwrap();
-    assert_eq!(v, 456);
-}
+    /// Arrays in Squirrel are 0-based and not 1-cringed.
+    #[test]
+    fn array_get() {
+        let sq = Squirrel::new(1024);
+        let arr: Array<'_> = sq.eval("return [123, 456, 789]").unwrap();
+        let v: Integer = arr.get(1).unwrap().unwrap();
+        assert_eq!(v, 456);
+    }
 
-#[test]
-fn array_set() {
-    use crate::Squirrel;
+    #[test]
+    fn array_set() {
+        let sq = Squirrel::new(1024);
+        let arr: Array<'_> = sq.eval("return [123, 456, 789]").unwrap();
+        arr.set(1, 444).unwrap();
+        let v: Integer = arr.get(1).unwrap().unwrap();
+        assert_eq!(v, 444);
+    }
 
-    let sq = Squirrel::new(1024);
-    let arr: Array<'_> = sq.eval("return [123, 456, 789]").unwrap();
-    arr.set(1, 444).unwrap();
-    let v: Integer = arr.get(1).unwrap().unwrap();
-    assert_eq!(v, 444);
+    /// Arrays in Squirrel have to be grown manually.
+    /// Setting an array index out of bounds will cause
+    /// a runtime error.
+    #[test]
+    fn array_set_oob() {
+        let sq = Squirrel::new(1024);
+        let arr: Array<'_> = sq.eval("return [123, 456, 789]").unwrap();
+        let err = arr.set(4, 444).unwrap_err();
+        assert!(matches!(err, CallError::Runtime(Value::String(_))))
+    }
+
+    #[test]
+    fn array_append() {
+        let sq = Squirrel::new(1024);
+        let arr: Array<'_> = sq.eval("return [123, 456, 789]").unwrap();
+        arr.append(321).unwrap();
+        let v: Integer = arr.get(3).unwrap().unwrap();
+        assert_eq!(v, 321);
+    }
+
+    #[test]
+    fn array_pop() {
+        let sq = Squirrel::new(1024);
+        let arr: Array<'_> = sq.eval("return [123, 456, 789]").unwrap();
+        let v: Integer = arr.pop().unwrap();
+        assert_eq!(v, 789);
+        assert_eq!(arr.len(), 2);
+    }
+
+    #[test]
+    fn array_len() {
+        let sq = Squirrel::new(1024);
+        let arr: Array<'_> = sq.eval("return [123, 456, 789]").unwrap();
+        assert_eq!(arr.len(), 3);
+    }
 }
